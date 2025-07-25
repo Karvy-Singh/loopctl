2dc99963ba72cdae4d1cf7736dedc549f695fd7a
Karvy Singh <karvysingh14@gmail.com>
2025-07-25 13:43:40 +0530

    init: installation script linux

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+
+echo "Compiling & Installing..."
+gcc -o loopctl loopctl_linux.c utils/utils.c $(pkg-config --cflags --libs dbus-1)
+sudo cp bar loopctl /usr/local/bin/
+echo "Installation complete"
+echo "usage:\n loopctl (for repeating full current media infinite times)\n loopctl N (for repeating full current media N times)\n loopctl -p START END (for repeating current media from START point to END point infinite times)\n loopctl -p START END N (for repeating current media from START point to END point N times)"
