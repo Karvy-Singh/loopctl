89bb7bfb5532b51c8e3380f8a19e6d0fbe5921bb
Karvy Singh <karvysingh14@gmail.com>
2025-07-25 12:11:25 +0530

    feat: infinite loop working with timer

--- loopctl_linux.c
+++ loopctl_linux.c
@@ -1,6 +1,7 @@
 #include "utils/utils.h"
 #include <dbus/dbus.h>
 #include <errno.h>
+#include <inttypes.h>
 #include <poll.h>
 #include <signal.h>
 #include <stdint.h>
@@ -15,7 +16,13 @@ static DBusConnection *conn;
 static const char *player; // the MPRIS player we’re looping
 static int timer_fd;
 static int64_t start_us, end_us;
-static long loops_left = -1;
+int loop_count = 0;
+int max_loops = 0;
+
+void handle_sigint(int sig) {
+  (void)sig;
+  keep_running = 0;
+}
 
 static void arm_timer(int64_t now_us) {
   if (now_us >= end_us)
@@ -29,26 +36,6 @@ static void arm_timer(int64_t now_us) {
     perror("timerfd_settime");
 }
 
-static inline void on_segment_finished(void) {
-  if (loops_left == 0) {
-    // we're done – stop the timer and break the main loop
-    struct itimerspec off = {{0}, {0}};
-    timerfd_settime(timer_fd, 0, &off, NULL);
-    keep_running = 0;
-  } else {
-    if (loops_left > 0)
-      --loops_left;
-    set_position(conn, player, start_us);
-    int64_t pos = get_position(conn, player);
-    arm_timer(pos);
-  }
-}
-
-void handle_sigint(int sig) {
-  (void)sig;
-  keep_running = 0;
-}
-
 static DBusHandlerResult filter_cb(DBusConnection *c, DBusMessage *m,
                                    void *userdata) {
   (void)c;
@@ -56,66 +43,47 @@ static DBusHandlerResult filter_cb(DBusConnection *c, DBusMessage *m,
 
   if (dbus_message_is_signal(m, "org.freedesktop.DBus.Properties",
                              "PropertiesChanged")) {
-    const char *iface = NULL;
-    DBusMessageIter iter;
-
-    if (!dbus_message_iter_init(m, &iter))
-      return DBUS_HANDLER_RESULT_HANDLED;
-    if (dbus_message_iter_get_arg_type(&iter) != DBUS_TYPE_STRING)
-      return DBUS_HANDLER_RESULT_HANDLED;
-
+    const char *iface;
+    DBusMessageIter iter, dict;
+    dbus_message_iter_init(m, &iter);
     dbus_message_iter_get_basic(&iter, &iface);
-    if (strcmp(iface, "org.mpris.MediaPlayer2.Player") != 0)
-      return DBUS_HANDLER_RESULT_HANDLED;
+    if (strcmp(iface, "org.mpris.MediaPlayer2.Player") == 0) {
+      dbus_message_iter_next(&iter);
+      dbus_message_iter_recurse(&iter, &dict);
 
-    // map is next argument
-    dbus_message_iter_next(&iter);
-    if (dbus_message_iter_get_arg_type(&iter) != DBUS_TYPE_ARRAY)
-      return DBUS_HANDLER_RESULT_HANDLED;
+      while (dbus_message_iter_get_arg_type(&dict) != DBUS_TYPE_INVALID) {
+        DBusMessageIter entry, variant;
+        const char *prop;
+        dbus_message_iter_recurse(&dict, &entry);
+        dbus_message_iter_get_basic(&entry, &prop);
 
-    DBusMessageIter dict;
-    dbus_message_iter_recurse(&iter, &dict);
+        if (strcmp(prop, "PlaybackStatus") == 0) {
+          dbus_message_iter_next(&entry);
+          dbus_message_iter_recurse(&entry, &variant);
+          const char *status;
+          dbus_message_iter_get_basic(&variant, &status);
 
-    while (dbus_message_iter_get_arg_type(&dict) != DBUS_TYPE_INVALID) {
-      DBusMessageIter entry, variant;
-      const char *prop = NULL;
-
-      dbus_message_iter_recurse(&dict, &entry); // dict entry (key, value)
-      if (dbus_message_iter_get_arg_type(&entry) != DBUS_TYPE_STRING) {
-        dbus_message_iter_next(&dict);
-        continue;
-      }
-
-      dbus_message_iter_get_basic(&entry, &prop);
-      if (strcmp(prop, "PlaybackStatus") == 0) {
-        dbus_message_iter_next(&entry); // move to variant
-        dbus_message_iter_recurse(&entry, &variant);
-        const char *status = NULL;
-        dbus_message_iter_get_basic(&variant, &status);
-
-        if (status && strcmp(status, "Playing") == 0) {
-          int64_t pos = get_position(conn, player);
-          arm_timer(pos);
-        } else {
-          struct itimerspec off = {{0}, {0}};
-          timerfd_settime(timer_fd, 0, &off, NULL);
+          if (strcmp(status, "Playing") == 0) {
+            int64_t pos = get_position(conn, player);
+            arm_timer(pos);
+          } else {
+            /* pause: stop the timer */
+            struct itimerspec off = {{0}, {0}};
+            timerfd_settime(timer_fd, 0, &off, NULL);
+          }
         }
-      }
 
-      dbus_message_iter_next(&dict);
+        dbus_message_iter_next(&dict);
+      }
     }
 
   } else if (dbus_message_is_signal(m, "org.mpris.MediaPlayer2.Player",
                                     "Seeked")) {
-    int64_t new_pos = 0;
+    int64_t new_pos;
     if (dbus_message_get_args(m, NULL, DBUS_TYPE_INT64, &new_pos,
                               DBUS_TYPE_INVALID)) {
       if (new_pos >= end_us) {
-        // user dragged beyond our end – snap back but DO NOT change loops_left
-        // here
         set_position(conn, player, start_us);
-        int64_t pos = get_position(conn, player);
-        arm_timer(pos);
       } else {
         arm_timer(new_pos);
       }
@@ -125,25 +93,14 @@ static DBusHandlerResult filter_cb(DBusConnection *c, DBusMessage *m,
   return DBUS_HANDLER_RESULT_HANDLED;
 }
 
-void usage() {
-  char *prog = "loopctl";
-  fprintf(stderr,
-          "Usage:\n"
-          "  %s                 # loop full track infinitely\n"
-          "  %s N               # loop full track N times\n"
-          "  %s -p START END    # loop [START,END] infinitely\n"
-          "  %s -p START END X  # loop [START,END] X times\n"
-          "  (START, END in seconds)\n",
-          prog, prog, prog, prog);
-  exit(EXIT_FAILURE);
-}
-
 int main(int argc, char *argv[]) {
   signal(SIGINT, handle_sigint);
 
-  conn = connect_session_bus();
+  DBusError err;
+  dbus_error_init(&err);
+  conn = dbus_bus_get(DBUS_BUS_SESSION, &err);
   if (!conn) {
-    fprintf(stderr, "Failed to connect to session bus\n");
+    fprintf(stderr, "Failed to connect to session bus: %s\n", err.message);
     return EXIT_FAILURE;
   }
 
@@ -175,40 +132,28 @@ int main(int argc, char *argv[]) {
     return EXIT_FAILURE;
   }
 
-  // Parse command line arguments
   if (argc == 1) {
     // loopctl - full, infinite
     start_us = 0;
     end_us = get_track_length(conn, player);
+    set_position(conn, player, start_us);
 
-    while (keep_running) {
-      set_position(conn, player, start_us);
-      usleep((end_us - start_us) / 1000000 *
-             1000000); // Wait for track duration
-    }
   } else if (argc == 2) {
     // loopctl 5 - full, 5 times
     int times = atoi(argv[1]);
     start_us = 0;
     end_us = get_track_length(conn, player);
+    max_loops = times;
+    set_position(conn, player, start_us);
 
-    for (int loop = 0; loop < times && keep_running; loop++) {
-      set_position(conn, player, start_us);
-      usleep((end_us - start_us) / 1000000 *
-             1000000); // Wait for track duration
-    }
   } else if (argc == 4) {
     // loopctl part START END - partial, infinite
     int s = atoi(argv[2]);
     int e = atoi(argv[3]);
     start_us = (int64_t)s * 1000000;
     end_us = (int64_t)e * 1000000;
+    set_position(conn, player, start_us);
 
-    while (keep_running) {
-      set_position(conn, player, start_us);
-      usleep((end_us - start_us) / 1000000 *
-             1000000); // Wait for segment duration
-    }
   } else if (argc == 5) {
     // loopctl part START END X - partial, X times
     int s = atoi(argv[2]);
@@ -216,12 +161,9 @@ int main(int argc, char *argv[]) {
     int times = atoi(argv[4]);
     start_us = (int64_t)s * 1000000;
     end_us = (int64_t)e * 1000000;
+    max_loops = times;
+    set_position(conn, player, start_us);
 
-    for (int loop = 0; loop < times && keep_running; loop++) {
-      set_position(conn, player, start_us);
-      usleep((end_us - start_us) / 1000000 *
-             1000000); // Wait for segment duration
-    }
   } else {
     fprintf(stderr, "Usage:\n");
     fprintf(stderr, "  %s                    # full, infinite\n", argv[0]);
@@ -231,9 +173,90 @@ int main(int argc, char *argv[]) {
     goto cleanup;
   }
 
+  dbus_bus_add_match(conn,
+                     "type='signal',"
+                     "interface='org.freedesktop.DBus.Properties',"
+                     "member='PropertiesChanged',"
+                     "arg0='org.mpris.MediaPlayer2.Player'",
+                     NULL);
+  dbus_bus_add_match(conn,
+                     "type='signal',"
+                     "interface='org.mpris.MediaPlayer2.Player',"
+                     "member='Seeked'",
+                     NULL);
+  dbus_connection_add_filter(conn, filter_cb, NULL, NULL);
+
+  timer_fd = timerfd_create(CLOCK_MONOTONIC, 0);
+  if (timer_fd < 0) {
+    perror("timerfd_create");
+    goto cleanup;
+  }
+
+  char *st = get_playback_status(conn, player);
+  if (st && strcmp(st, "Playing") == 0) {
+    int64_t pos = get_position(conn, player);
+    arm_timer(pos);
+  }
+  free(st);
+
+  int dbus_fd;
+  if (!dbus_connection_get_unix_fd(conn, &dbus_fd)) {
+    fprintf(stderr, "Failed to get D-Bus unix fd\n");
+    close(timer_fd);
+    goto cleanup;
+  }
+
+  struct pollfd pfds[2];
+  while (keep_running) {
+    pfds[0].fd = dbus_fd;
+    pfds[0].events = POLLIN;
+    pfds[1].fd = timer_fd;
+    pfds[1].events = POLLIN;
+
+    if (poll(pfds, 2, -1) < 0 && errno != EINTR) {
+      perror("poll");
+      break;
+    }
+
+    if (pfds[0].revents & POLLIN)
+      dbus_connection_read_write_dispatch(conn, 0);
+
+    if (pfds[1].revents & POLLIN) {
+      uint64_t expirations = 0;
+      if (read(timer_fd, &expirations, sizeof(expirations)) > 0) {
+
+        /* Sanity: timerfd is one-shot, but just in case… */
+        if (expirations == 0)
+          expirations = 1;
+
+        /* Count how many times we've ALREADY finished the segment */
+        loop_count += (int)expirations;
+        fprintf(stderr, "[loopctl] segment done (%d/%d)\n", loop_count,
+                max_loops);
+
+        /* If user asked for a finite number and we just finished the last one,
+         * stop */
+        if (max_loops > 0 && loop_count >= max_loops) {
+          fprintf(stderr, "[loopctl] reached max loops, exiting\n");
+          keep_running = 0;
+          continue; /* IMPORTANT: do NOT seek back once we’re done */
+        }
+
+        /* Otherwise: jump back and re-arm */
+        set_position(conn, player, start_us);
+        arm_timer(start_us);
+        fprintf(stderr, "[loopctl] rewound to %" PRId64 " µs\n", start_us);
+      }
+    }
+  }
+
+  close(timer_fd);
+  dbus_connection_remove_filter(conn, filter_cb, NULL);
+
 cleanup:
   for (int i = 0; i < count; i++)
     free(players[i]);
   free(players);
+
   return EXIT_SUCCESS;
 }
